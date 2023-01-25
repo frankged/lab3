4dbd4ec83fc756287a0c7265691a8cc1aa6512b4
Frank <fdrugge@ucsd.edu>
2023-01-25 13:40:18 -0800

    Bug fixes on reversed and reverseInPlace

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,6 +8,12 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+  @Test 
+	public void testReverseInPlaceFails() {
+    int[] input1 = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input1);
+	}
 
 
   @Test
